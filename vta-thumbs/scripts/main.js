9e66b2257d4178b1364fe9cb80f739ee611179e2
Yuhei Jotaki <yuheijotaki@gmail.com>
2023-09-03 15:15:24 +0900

    add: cats

--- vta-thumbs/scripts/main.js
+++ vta-thumbs/scripts/main.js
@@ -1,7 +1,7 @@
 import { getPageContent, onLinkNavigate, transitionHelper, getLink } from "./utils.js";
 
 const galleryPath = "index.html";
-const detailPath = `detail-`;
+const detailPath = `cat_`;
 
 /**
  * 遷移先の種類を判定します。
@@ -34,7 +34,7 @@ onLinkNavigate(async ({ fromPath, toPath, isBack }) => {
 
   if (navigationType === "index-to-detail-page") {
     targetThumbnail = getLink(toPath).querySelector("img");
-    targetThumbnail.style.viewTransitionName = "banner-img";
+    targetThumbnail.style.viewTransitionName = "cat-anime";
     classNames = "from-index to-detail";
   } else if (navigationType === "detail-page-to-index") {
     classNames = "to-index from-detail";
@@ -48,7 +48,7 @@ onLinkNavigate(async ({ fromPath, toPath, isBack }) => {
 
       if (navigationType === "detail-page-to-index") {
         targetThumbnail = getLink(fromPath).querySelector("img");
-        targetThumbnail.style.viewTransitionName = "banner-img";
+        targetThumbnail.style.viewTransitionName = "cat-anime";
       }
     },
     classNames,
@@ -56,19 +56,5 @@ onLinkNavigate(async ({ fromPath, toPath, isBack }) => {
 
   transition.finished.finally(() => {
     if (targetThumbnail) targetThumbnail.style.viewTransitionName = "";
-  });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  // 初期表示時の演出
-  document.querySelectorAll(".photo-list > li").forEach((element, index) => {
-    element.style = "opacity: 0";
-    element.animate([{ scale: 0.9 }, { opacity: 1 }], {
-      duration: 500,
-      delay: 75 * index + 100,
-      fill: "forwards",
-      // back-out
-      easing: "cubic-bezier(0.175, 0.885, 0.32, 1.275)",
-    });
   });
 });
